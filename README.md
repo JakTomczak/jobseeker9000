53200f077ff4be805de1f7302a4eb95529286a16
Jakub Tomczak <jak.tomczak@gmail.com>
2019-12-08 21:18:08 +0100

    Added some documentation.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+Web scrapper for searching for job offers accross the internet.
+
+It is very far away from being finished.
+
 Let's start with pracuj.pl
--- lib/jobseeker9000/floker/pracuj/search.ex
+++ lib/jobseeker9000/floker/pracuj/search.ex
@@ -1,12 +1,21 @@
 defmodule Jobseeker9000.Floker.Pracuj.Search do
   alias Jobseeker9000.Floker.Pracuj
   
+  """
+  bigcontainer - The html marker that holds all information needed for scrapping.
+  one_result_container - The html markers pattern,
+    that each of corresponding markers hold all information about one offer.
+  """
   @bigcontainer "div#results ul.results__list-container"
   @one_result_container "li.results__list-container-item"
 
   defp unpack!({_, _, [inside]}), do: inside
   defp unpack!([value]), do: value
 
+  @doc """
+  Scraps full html to find bigcontainer and than changes scope to this marker.
+  If bigcontainer is not found, returns an error.
+  """
   defp get_results(%Pracuj{state: :error} = pracuj), do: pracuj
   defp get_results(%Pracuj{state: :ok, full_html: html} = pracuj) do
     case Floki.find(html, @bigcontainer) do
@@ -15,29 +24,42 @@ defmodule Jobseeker9000.Floker.Pracuj.Search do
     end
   end
 
+  @doc """
+  Scraps bigcontainer to find list of one_result_container-s.
+  """
   defp find_lis(%Pracuj{state: :error} = pracuj), do: pracuj
   defp find_lis(%Pracuj{state: :ok, full_html: html} = pracuj) do
     %{ pracuj | all_the_lis: Floki.find(html, @one_result_container) }
   end
 
+  @doc """
+  Test function to perform actions only on the first one_result_container.
+  """
   defp test_on_first(%Pracuj{state: :error} = pracuj), do: pracuj
   defp test_on_first(%Pracuj{state: :ok, all_the_lis: []} = pracuj), do: pracuj
   defp test_on_first(%Pracuj{state: :ok, all_the_lis: [h | _]} = pracuj) do
     %{pracuj | all_the_lis: [h]}
   end
 
+  @doc """
+  Recursive function to perform actions on each one_result_container.
+  When stumbled across an error, stops the recursion and returns this error.
+  """
   defp li_crawler(%Pracuj{state: :error} = pracuj), do: pracuj
   defp li_crawler(%Pracuj{state: :ok, all_the_lis: []} = pracuj), do: pracuj
   defp li_crawler(%Pracuj{state: :ok, all_the_lis: [head | tail], results: results} = pracuj) do
     case for_each_li(head) do
       %{error: nil, value: nil, relative_link: relative_link, full_link: full_link, id: id} ->
-        offert = %{relative_link: relative_link, full_link: full_link, id: id}
-        li_crawler( %{ pracuj | all_the_lis: tail, results: [offert | results] } )
+        offer = %{relative_link: relative_link, full_link: full_link, id: id}
+        li_crawler( %{ pracuj | all_the_lis: tail, results: [offer | results] } )
       %{error: error_text, value: value} ->
         %{ pracuj | state: :error, error_text: error_text, error_values: value }
     end
   end
 
+  @doc """
+  Performs list of actions on every one_result_container.
+  """
   defp for_each_li({_, _, [inside]}) do
     case Floki.find(inside, "div.offer-details__text") do
       [] -> %{error: "No div with class 'offer-details__text' found.", value: %{parent_tree: inside}}
@@ -45,6 +67,9 @@ defmodule Jobseeker9000.Floker.Pracuj.Search do
     end
   end
 
+  @doc """
+  Scraps every marker to find the actual data.
+  """
   defp extract_basic_info_from_div(div) do
     %{error: nil, value: nil, relative_link: "", full_link: "", id: -1}
     |> href_given_class(div, "offer-details__title-link")
@@ -52,6 +77,11 @@ defmodule Jobseeker9000.Floker.Pracuj.Search do
     |> id_from_relative()
   end
 
+  @doc """
+  Finds the url of the offer.
+  Returns error if no hyperlink is found within given marker
+    or if more than one such hyperlink is found.
+  """
   defp href_given_class(%{error: error} = values, _, _) when is_binary(error), do: values
   defp href_given_class(%{error: nil} = values, html, class) do
     nodes = Floki.attribute(html, "a.#{class}", "href")
@@ -62,11 +92,17 @@ defmodule Jobseeker9000.Floker.Pracuj.Search do
     end
   end
 
+  @doc """
+  Changes relative link to a full one.
+  """
   defp full_link_from_relative(%{error: error} = values) when is_binary(error), do: values
   defp full_link_from_relative(%{error: nil, relative_link: relative_link} = values) do
     %{values | full_link: Pracuj.website() <> relative_link}
   end
 
+  @doc """
+  Finds internal id of a pracuj.pl offer. What for? Idunno.
+  """
   defp id_from_relative(%{error: error} = values) when is_binary(error), do: values
   defp id_from_relative(%{error: nil, relative_link: relative_link} = values) do
     splitted = String.split(relative_link, ",oferta,")
